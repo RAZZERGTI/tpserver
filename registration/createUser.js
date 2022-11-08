2405528a28a987ea7c543c0b048e7a32b6a05269
unknown <RAZZERGTI>
2022-11-08 15:46:35 +0300

    add some changes in reduction

--- registration/createUser.js
+++ registration/createUser.js
@@ -4,7 +4,6 @@ function randomInteger(min, max) {
 	let rand = min - 0.5 + Math.random() * (max - min + 1)
 	return Math.round(rand)
 }
-<<<<<<< HEAD
 async function registrationUser(href) {
 	let validationNameAndMail = /\/\w+\/\w+\?([a-z]+)?=(\w+@?\w+\.?\w+)?/i
 	let resultRegular = href.match(validationNameAndMail)
@@ -87,88 +86,6 @@ async function registrationUser(href) {
 			}
 		}
 	}
-=======
-async function registrationUser(href){
-    let validationNameAndMail = /\/\w+\/\w+\?([a-z]+)?=(\w+@?\w+\.?\w+)?/i
-    let resultRegular = href.match(validationNameAndMail)
-    let info = resultRegular[2]
-    if (resultRegular[1] === 'name'){
-        let reg = /\/\w+\/\w+\?([a-z]+)?=?([a-z\d]+)?&?([a-z]+)?=?([a-z\d-]+\.*[a-z\d_-]+@[a-z]+.[a-z]+)?&?([a-z]+)?=?([A-Za-z\d]+)?/i
-        let result = href.match(reg)
-        let name = result[2]
-        let mail = result[4]
-        let password = result[6]
-        if (password != null){
-            let sessionCheck = await db.infoCheckDb('registration','name',name)
-            if (sessionCheck){
-                return {"error": {
-                        "statusCode": 401,
-                        "name": "unAuthorized",
-                        "message": 'user and password already in session'
-                    }}
-            }
-            else{
-                let idUser = randomInteger(100000000, 999999999)
-                let code = randomInteger(10000, 99999)
-                const arrCode = [idUser, `${name}`, `${mail}`, `${password}`, code]
-                const createDbCode = await db.createDbCode(arrCode)
-                const sessCode = await session.sessionCode('registration','mail',mail, code)
-                return {
-                    response: {
-                        id: idUser,
-                        name: name,
-                        mail: mail
-                    }
-                }
-            }
-        }
-        else if(mail != null) {
-            return {
-                "error": {
-            "statusCode": 400,
-            "name": "Bad request",
-            "message": 'fields are empty'
-            }}
-        }
-        else {
-            let users = await db.infoCheckDb('users','name',info)
-            let session = await db.infoCheckDb('registration','name',info)
-            if (users || session){
-                return {
-                    response: false
-                }
-            }
-            else{
-                return {
-                    response: true
-                }
-            }
-        }
-    }
-    else if(resultRegular[1] === 'mail'){
-        let users = await db.infoCheckDb('users','mail',info)
-        let session = await db.infoCheckDb('registration','mail',info)
-        if (users || session){
-            return {
-                response: false
-            }
-        }
-        else{
-            return {
-                response: true
-            }
-        }
-    }
-    else{
-        return {
-            "error": {
-                "statusCode": 400,
-                "name": "Bad request",
-                "message": 'Change parameters'
-            }}
-    }
-
->>>>>>> 4e3448e013b9b8d3ee0993100fdd52183c4d2126
 }
 module.exports = {
 	registrationUser,
