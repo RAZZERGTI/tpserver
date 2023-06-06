3ee7bb342e0086f58222fb7a58ee1cc6f3267c70
RAZZERGTI <78425187+RAZZERGTI@users.noreply.github.com>
2023-06-06 17:52:03 +0300

    Update getFolders.js

--- zoho/album/getFolders/getFolders.js
+++ zoho/album/getFolders/getFolders.js
@@ -21,8 +21,9 @@ const getAllFolders = async (req, token) => {
 			.then(async data => {
 				if (data.length > 0) {
 					const ids = data.map(item => item.id)
+					console.log('IDS --- ',ids)
 					let obj = await getTitle(ids, idUser).reverse()
-
+					console.log('OBJ --- ',obj)
 					resolve(obj)
 				} else {
 					resolve({})
